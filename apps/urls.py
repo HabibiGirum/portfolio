d56026158f19f8574219fe8badfd32e0f5724a58
HabibiGirum <habtamugirum0489@gmail.com>
2025-01-03 17:19:29 +0300

    write resume and index

--- apps/urls.py
+++ apps/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/about',views.about, name='about'),
+    # path('', views.index, name='index'),
+    path('',views.about, name='about'),
     path('/resume', views.resume, name='resume'),
     path('/contact', views.contact, name='contact'),
     path('/portfolio', views.portfolio, name='portfolio'),
